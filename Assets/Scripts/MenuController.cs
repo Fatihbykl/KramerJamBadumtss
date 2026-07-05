f9c5f837ebeccfa8347973b9708d8163ff9db233
FantaLaTone <fantalatone@gmail.com>
2026-07-05 18:00:51 +0300

    added menu design

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -6,9 +8,19 @@ public class MenuController : MonoBehaviour
 {
     public Button StartButton, QuitButton;
 
+    public TextMeshProUGUI title;
+
+    private float timer;
+
     private void Start()
     {
         StartButton.onClick.AddListener(() => SceneManager.LoadScene("Game"));
         QuitButton.onClick.AddListener(() => Application.Quit());
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        title.color = new Color(Mathf.Lerp(100f, 255f, Mathf.Sin(timer)) / 255f, 0f, 0f);
     }
 }
